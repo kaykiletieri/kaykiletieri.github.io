8f31e07e9f2924b3c298ea8ec3bcb6bb39e25276
Kayki Letieri <kaykiletieri37@gmail.com>
2022-10-31 20:00:27 -0300

    :sparkles: feat: header

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert("Estou em desenvolvimento, volte mais tarde!")
+//alert("Estou em desenvolvimento, volte mais tarde!")
+
+//Header
+const btnMobile = document.querySelector('.header__btn');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+
+    const nav = document.querySelector('.header__nav');
+    nav.classList.toggle('--active');
+
+    const active = nav.classList.contains('--active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
