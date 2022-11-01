22c689c69f79a868e405de8b32344784f4d87cb1
Kayki Letieri <kaykiletieri37@gmail.com>
2022-10-31 21:59:09 -0300

    :sparkles: feat: section home

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+//Home
+
+function typeWriter (elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 150 * i);
+    });
+}
+
+typeWriter (document.querySelector('.home__titulo'));
