dd48dc0d1e86db02423ad284ad613546af4f7eca
Kayki Letieri <kaykiletieri37@gmail.com>
2022-11-04 22:33:40 -0300

    :lipstick: feat: nav scroll

--- script.js
+++ script.js
@@ -22,6 +22,27 @@ function toggleMenu(event) {
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
 
+
+let section = document.querySelectorAll("section");
+let menu = document.querySelectorAll(".header__link");
+
+window.onscroll = () => {
+  section.forEach((i) => {
+    let top = window.scrollY;
+    let offset = i.offsetTop - 150;
+    let height = i.offsetHeight;
+    let id = i.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      menu.forEach((link) => {
+        link.classList.remove("--active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("--active");
+      });
+    }
+  });
+};
 //Home
 
 function typeWriter (elemento) {
